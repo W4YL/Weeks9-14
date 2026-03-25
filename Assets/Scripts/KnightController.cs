7ce4989b192a5f0d02bd76d035cb925e36e700ef
Zixi Yan <yanzix@shernet.sheridancollege.ca>
2026-03-25 11:27:28 -0400

    Made the most hideous thing ever

--- Assets/Scripts/KnightController.cs
+++ Assets/Scripts/KnightController.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class KnightController : MonoBehaviour
 {
     public AudioSource stepSound;
     public ParticleSystem stepParticleSystem;
+    public List<AudioClip> stepSoundClips;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,7 +22,13 @@ public class KnightController : MonoBehaviour
     public void FootStep()
     {
         Debug.Log("Step!");
-        stepSound.Play();
-        stepParticleSystem.Play();
+        stepParticleSystem.Emit(10);
+
+        stepSound = transform.GetComponent<AudioSource>();
+        int randomNum;
+        randomNum = Random.Range(0, 4);
+        stepSound.clip = stepSoundClips[randomNum];
+        stepSound.pitch = Random.Range(0.5f, 4);
+        stepSound.PlayOneShot(stepSound.clip);
     }
 }
