357515b72869458b3238e939f63c7257775a1dcd
W4ckyYL <yanzixi.w4yl@gmail.com>
2026-04-04 19:29:39 -0400

    Added gravity physics and ground collision

--- Assets/1_SystemProject/Scripts/PlayerController.cs
+++ Assets/1_SystemProject/Scripts/PlayerController.cs
@@ -1,13 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public SpriteRenderer playerHitbox;
-    public SpriteRenderer cameraLock;
+    public SpriteRenderer hbBottomCheck;
+    public SpriteRenderer hbLeftCheck;
+    public SpriteRenderer hbRightCheck;
+    public List<SpriteRenderer> environmentHitbox;
+
+    public Transform cameraLock;
 
     public float speed = 5;
     public Vector2 movement;
+    public Vector2 velocity;
+    public float gravity = 1;
+    public bool isGrounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,12 +27,41 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 newPosition = transform.position;
-        newPosition.x += movement.x * speed * Time.deltaTime;
-        transform.position = newPosition;
+        //Vector2 newPosition = transform.position;
+        //newPosition.x += movement.x * speed * Time.deltaTime;
+        //transform.position = newPosition;
+
+        velocity.x = movement.x * speed;
+
+        if (!isGrounded)
+            velocity.y -= gravity * Time.deltaTime;
+
+        transform.position += (Vector3)(velocity * Time.deltaTime);
+
+        HitboxCheck();
+    }
+
+    public void HitboxCheck()
+    {
+        foreach (SpriteRenderer hbSr in environmentHitbox)
+        {
+            if (playerHitbox.bounds.Intersects(hbSr.bounds))
+            {
+                if (hbBottomCheck.bounds.Intersects(hbSr.bounds))
+                {
+                    velocity.y = 0;
+                    isGrounded = true;
+                }
+            }
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
     {
         movement = context.ReadValue<Vector2>();
     }
