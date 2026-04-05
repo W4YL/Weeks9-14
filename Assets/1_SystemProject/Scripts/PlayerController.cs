d3d9787d08dde193d0c9b018fcc40acd91d63bb5
W4ckyYL <yanzixi.w4yl@gmail.com>
2026-04-04 20:46:04 -0400

    Rebuilt environment for easier physics implementation

--- Assets/1_SystemProject/Scripts/PlayerController.cs
+++ Assets/1_SystemProject/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public SpriteRenderer hbLeftCheck;
     public SpriteRenderer hbRightCheck;
     public List<SpriteRenderer> environmentHitbox;
+    public List<Transform> environmentHitboxTransform;
 
     public Transform cameraLock;
 
@@ -38,7 +39,19 @@ public class PlayerController : MonoBehaviour
 
         transform.position += (Vector3)(velocity * Time.deltaTime);
 
+        isGrounded = false;
+
         HitboxCheck();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+
     }
 
     public void HitboxCheck()
@@ -51,18 +64,37 @@ public class PlayerController : MonoBehaviour
                 {
                     velocity.y = 0;
                     isGrounded = true;
+
+
+                    Transform hbTr = hbSr.transform;
+
+                    Vector2 groundHeight = transform.position;
+                    groundHeight.y = hbSr.bounds.max.y + hbTr.localScale.y / 2;
+                    transform.position = groundHeight;
+                }
+
+                else if (hbLeftCheck.bounds.Intersects(hbSr.bounds))
+                {
+                    velocity.x = 0;
+
+                    Transform hbTr = hbSr.transform;
+
+                    Vector2 wallRestraintL = transform.position;
+                    wallRestraintL.x = hbSr.bounds.max.x + hbTr.localScale.x / 4;
+                    transform.position = wallRestraintL;
+                }
+
+                else if (hbRightCheck.bounds.Intersects(hbSr.bounds))
+                {
+                    velocity.x = 0;
+
+                    Transform hbTr = hbSr.transform;
+
+                    Vector2 wallRestraintR = transform.position;
+                    wallRestraintR.x = hbSr.bounds.max.x - hbTr.localScale.x / 4;
+                    transform.position = wallRestraintR;
                 }
             }
         }
-    }
-
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        movement = context.ReadValue<Vector2>();
-    }
-
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        movement = context.ReadValue<Vector2>();
     }
 }
